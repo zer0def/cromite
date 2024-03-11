fe0c1cba97705aea6759ec59a5fbc9c91e6d03e3
Carmelo Messina <uazo@users.noreply.github.com>
2024-03-11 17:58:53 +0100

    Update patches doc

--- tools/extract-all-patch-data.sh
+++ tools/extract-all-patch-data.sh
@@ -3,14 +3,14 @@
 FOLDER=$1
 SCRIPT_FOLDER="$(dirname "$0")"
 
-OUTPUT="$SCRIPT_FOLDER"/../docs/patches.md
+OUTPUT="$SCRIPT_FOLDER"/../docs/PATCHES.md
 test -f $OUTPUT && rm $OUTPUT
 
 for filename in "$FOLDER"/*.patch; do
     bash $SCRIPT_FOLDER/extract-patch-data.sh $filename >>$OUTPUT
 done
 
-sort -k2 -t"|" -o $OUTPUT $OUTPUT
+sort -k1 -t"|" -o $OUTPUT $OUTPUT
 
 sed -i '1s/^/| Patch | Message |\n/' $OUTPUT
 sed -i '2s/^/|--------|--------|\n/' $OUTPUT
