abde6d117a4f983648e7f3c6f2114c412a95e2f3
Emil Velikov <emil.l.velikov@gmail.com>
2024-06-03 16:40:16 +0100

    tools: remove unused fields Category/Context
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- tools/extract-patch-data.sh
+++ tools/extract-patch-data.sh
@@ -15,17 +15,11 @@ do
     elif [[ $current_line =~ ^License:* ]]; then
         LICENSE=`echo ${current_line:9} | cut -d ' ' -f1`
 
-    elif [[ $current_line =~ ^Category:* ]]; then
-        CATEGORY=`echo ${current_line:9} | cut -d ' ' -f1`
-
     elif [[ $current_line =~ ^"Original License:"* ]]; then
         continue
 
     elif [[ $current_line =~ ^From:* ]]; then
         FROM=`echo ${current_line:5} | cut -d ' ' -f1`
-
-    elif [[ $current_line =~ ^Context:* ]]; then
-        CONTEXT=${current_line:8}
 
     elif [[ $current_line =~ ^---* ]]; then
         break
@@ -43,8 +37,6 @@ done
 # echo SUBJECT: $SUBJECT
 # echo DATE: $DATE
 # echo LICENSE: $LICENSE
-# echo CONTEXT: $CONTEXT
-# echo CATEGORY: $CATEGORY
 # echo -e MESSAGE: $MESSAGE
 
 SUBJECT=`echo $SUBJECT | xargs`
@@ -53,7 +45,6 @@ echo "|**$SUBJECT**" \
      "<br><sub><nobr>"$DATE"</nobr>" \
      "<br>File: [$(basename $PATCH)](/build/patches/$(basename $PATCH))" \
      "<br><nobr>Author: "$FROM"</nobr>" \
-     "<br><nobr>Context: "$CONTEXT"</nobr>" \
      "<br><nobr>License: "$LICENSE"</nobr>" \
      "|"$MESSAGE"|"
 )
